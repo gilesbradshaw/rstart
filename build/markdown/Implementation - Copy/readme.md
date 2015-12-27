fd8633ce4c2a716dc8a4b5c8e10e107d1618e033
giles <giles.bradshaw@sigyl.com>
2015-12-27 21:04:50 +0000

    included build

---
+++ build/markdown/Implementation - Copy/readme.md
@@ -0,0 +1,3 @@
+ # here is d2
+    
+    <div style='background:red'>ahhhhhhhhhhhhhhhhhh</div>
---
+++ secrets.js
@@ -0,0 +1,6 @@
+var secrets = {
+    GITHUB_CLIENT_ID: 'bba8d65b0cd5b2286ea2',
+    GITHUB_CLIENT_SECRET: 'a88e2bc8c1240539649703855935b4ebe5283006',
+};
+
+module.exports = secrets;
